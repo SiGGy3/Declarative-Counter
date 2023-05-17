27b638e7957bac19fb2a369d93a0c623577311ce
Justin Sigarroa <justinjwsig@gmail.com>
2023-05-17 11:45:26 -0700

    finish

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let [ counter, setCounter ] = useState(1);
+window.setCounter = setCounter;
+
+  return (
+    <div className="App">
+      <h1>{ counter }</h1>
+      <h4>{ counter }</h4>
+    </div>
+  );
+}
+
+export default App;
